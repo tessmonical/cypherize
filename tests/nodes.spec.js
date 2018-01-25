659961e66ea2385cb4d9d1322433e744f9880a76
Tess Monical <tess.monical@gmail.com>
2018-01-25 14:27:19 -0600

    sanity check tests

--- tests/nodes.spec.js
+++ tests/nodes.spec.js
@@ -1,3 +1,15 @@
-describe('Node Tests', function() {
-  expect()
+/* eslint prefer-arrow-callback: 0 */
+/* eslint-env node, mocha, chai */
+
+const { expect } = require('chai');
+
+describe('Node Tests', function () {
+  it('Sanity checks', function () {
+    expect(0).to.equal(0);
+    expect(2).not.to.equal(3);
+  });
+
+  it('', function () {
+
+  });
 });
