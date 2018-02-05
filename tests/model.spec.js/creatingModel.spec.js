6cfc79a82852dfb0007de65860ca58ca99987bb1
Tess Monical <tess.monical@gmail.com>
2018-02-05 10:02:57 -0600

    adds test and fixes that test's failure

--- tests/model.spec.js/creatingModel.spec.js
+++ tests/model.spec.js/creatingModel.spec.js
@@ -4,28 +4,33 @@
 const { expect } = require('chai');
 const { defineModel } = require('../../lib/model');
 
-describe('defineModel', function () {
+describe('Defining a Model', function () {
   let Model;
-  beforeEach(function () {
+  before(function () {
     Model = defineModel({
       name: 'character',
       fields: ['name', 'gender'],
     });
   });
 
-  it('is a function', function () {
+  it('defineModel is a function', function () {
     expect(defineModel).to.be.a('function');
   });
 
-  it('returns a function', function () {
+  it('defineModel returns a function', function () {
     expect(Model).to.be.a('function');
   });
 
-  it('returns function with correct prototype methods', function () {
+  it('defineModel returns function with correct prototype methods', function () {
     expect(Model.prototype.findAll).to.be.a('function');
     expect(Model.prototype.findOne).to.be.a('function');
     expect(Model.prototype.create).to.be.a('function');
     expect(Model.prototype.delete).to.be.a('function');
     expect(Model.prototype.setProperty).to.be.a('function');
+  });
+
+  it('defineModel returns function with correct properties', function () {
+    expect(Model._name).to.be.a('string');
+    expect(Model._fields).to.be.an('array');
   });
 });
